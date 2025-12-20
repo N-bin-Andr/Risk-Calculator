de56cf331a8322204cd8096c6f4c84edebec8ebf
Ahim <n.bin.andr@gmail.com>
2025-12-20 09:34:09 +0300

    feat: add grid entry support to calculator reducer
    - Added new state fields for grid calculation
    - Added gridEnabled flag to toggle grid mode
    - Added gridOrdersCount (1-10 orders limit)
    - Added gridDistribution array for % allocation
    - Added grid calculation results storage
    - Added 5 new action types for grid management
    - Implemented automatic distribution normalization

--- src/reducers/calculatorReducer.js
+++ src/reducers/calculatorReducer.js
@@ -1,79 +1,78 @@
 // calculatorReducer.js
 
-/* export const initialState = {
-    tpError: '',
-    slError: '',
-    isBacktest: false,
-    direction: '',
-    instrument: '',
-    entryPrice: '',
-    slPrice: '',
-    takeProfitPrice: '',
-    tpLevels: [{ price: '', percent: 100 }],
-    traderNote: '',
-    reportId: '',
-    date: '',
-    showReport: false,
-    vCoins: 0,
-    vValue: 0,
-    rrRatio: '',
-    riskValue: '',
-    slPoints: 0,
+/*export const initialState = {
+   tpError: '',
+   slError: '',
+   isBacktest: false,
+   direction: '',
+   instrument: '',
+   entryPrice: '',
+   slPrice: '',
+   takeProfitPrice: '',
+   tpLevels: [{ price: '', percent: 100 }],
+   traderNote: '',
+   reportId: '',
+   date: '',
+   showReport: false,
+   vCoins: 0,
+   vValue: 0,
+   rrRatio: '',
+   riskValue: '',
+   slPoints: 0,
 };
 
 export function calculatorReducer(state, action) {
-    switch (action.type) {
-        case 'SET_FIELD':
-            return {
-                ...state,
-                [action.field]: action.value,
-            };
-        case 'RESET_FORM':
-            return initialState;
+   switch (action.type) {
+       case 'SET_FIELD':
+           return {
+               ...state,
+               [action.field]: action.value,
+           };
+       case 'RESET_FORM':
+           return initialState;
 
-        case 'RESET_FIELDS_EXCEPT':
-            const resetState = {};
-            Object.keys(state).forEach(key => {
-                if (action.fieldsToKeep.includes(key)) {
-                    resetState[key] = state[key];
-                } else {
-                    const initial = initialState[key];
-                    resetState[key] =
-                        Array.isArray(initial) ? [] :
-                            typeof initial === 'number' ? 0 :
-                                typeof initial === 'boolean' ? false :
-                                    typeof initial === 'object' ? {} :
-                                        '';
-                }
-            });
-            return resetState;
+       case 'RESET_FIELDS_EXCEPT':
+           const resetState = {};
+           Object.keys(state).forEach(key => {
+               if (action.fieldsToKeep.includes(key)) {
+                   resetState[key] = state[key];
+               } else {
+                   const initial = initialState[key];
+                   resetState[key] =
+                       Array.isArray(initial) ? [] :
+                           typeof initial === 'number' ? 0 :
+                               typeof initial === 'boolean' ? false :
+                                   typeof initial === 'object' ? {} :
+                                       '';
+               }
+           });
+           return resetState;
 
 
-        case 'ADD_TP_LEVEL':
-            return {
-                ...state,
-                tpLevels: [...state.tpLevels, { price: '', percent: 0 }]
-            };
+       case 'ADD_TP_LEVEL':
+           return {
+               ...state,
+               tpLevels: [...state.tpLevels, { price: '', percent: 0 }]
+           };
 
-        case 'REMOVE_TP_LEVEL':
-            return {
-                ...state,
-                tpLevels: state.tpLevels.filter((_, i) => i !== action.index)
-            };
+       case 'REMOVE_TP_LEVEL':
+           return {
+               ...state,
+               tpLevels: state.tpLevels.filter((_, i) => i !== action.index)
+           };
 
-        case 'UPDATE_TP_LEVEL':
-            return {
-                ...state,
-                tpLevels: state.tpLevels.map((tp, i) =>
-                    i === action.index ? { ...tp, [action.field]: action.value } : tp
-                )
-            };
-        default:
-            return state;
-    }
+       case 'UPDATE_TP_LEVEL':
+           return {
+               ...state,
+               tpLevels: state.tpLevels.map((tp, i) =>
+                   i === action.index ? { ...tp, [action.field]: action.value } : tp
+               )
+           };
+       default:
+           return state;
+   }
 }
 */
-
 export const initialState = {
     tpError: '',
     slError: '',
